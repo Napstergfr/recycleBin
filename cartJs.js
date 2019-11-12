fed2cc7802bc15a6dd5ca7ab2d42913bb673a7e5
Napstergfr <gfr7oo@gmail.com>
2019-11-12 11:07:01 +0600

    3

--- cartJs.js
+++ cartJs.js
@@ -1,1 +1,11 @@
-document.write("Hi");
+$.getJSON( "/cart.js", function( data ) {
+		  var items = [];
+		  $.each( data, function( key, val ) {
+		    items.push( "<li id='" + key + "'>" + val + "</li>" );
+		  });
+		 
+		  $( "<ul/>", {
+		    "class": "my-new-list",
+		    html: items.join( "" )
+		  }).appendTo( "body" );
+		});
